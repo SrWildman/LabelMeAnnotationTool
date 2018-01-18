75dc2f84f49ba546c240f3c654df7263685e8b7f
WILDMAN <sw540q@att.com>
2018-01-18 12:24:18 -0600

    Change copy location for .htaccess to shell script

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp /var/www/html/AnnotationTool/Images/.htaccess /var/www/html/AnnotationTool/Annotations/.htaccess
 make -C /var/www/html/AnnotationTool/
 /etc/init.d/apache2 start
 tail -f /dev/null
