376afad62434c39f66b352ea09e8761e432b3db2
Wildman, Samuel (sw540q) <sw540q@att.com>
2018-07-06 13:14:06 -0500

    ignore dcm-jpg

--- make.sh
+++ make.sh
@@ -2,5 +2,5 @@
 cp /var/www/html/AnnotationTool/Images/.htaccess /var/www/html/AnnotationTool/Annotations/.htaccess
 make -C /var/www/html/AnnotationTool/
 /etc/init.d/apache2 start
-python3 /var/www/html/dcm-jpg.py /var/www/html/AnnotationTool/Images
+# python3 /var/www/html/dcm-jpg.py /var/www/html/AnnotationTool/Images
 tail -f /dev/null
