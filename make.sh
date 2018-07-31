77d610556f3466bf071674939d82133625642d1d
Wildman, Samuel (sw540q) <sw540q@att.com>
2018-07-31 15:26:44 -0500

    log anything to file

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp /var/www/html/AnnotationTool/Images/.htaccess /var/www/html/AnnotationTool/Annotations/.htaccess
-python3 /var/www/html/dcm-jpg.py /var/www/html/AnnotationTool/tmp_store /var/www/html/AnnotationTool/Images /var/www/html/AnnotationTool/Annotations
+python3 /var/www/html/dcm-jpg.py /var/www/html/AnnotationTool/tmp_store /var/www/html/AnnotationTool/Images /var/www/html/AnnotationTool/Annotations > /var/log/dcm_log
 make -C /var/www/html/AnnotationTool/
 /etc/init.d/apache2 start
 
